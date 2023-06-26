4859af548c0fedc9822014fea169cf8762e8f35f
Kent C. Dodds <me+github@kentcdodds.com>
2023-06-26 15:20:18 -0600

    rename

---
+++ exercises/01.styling/05.problem.bundling-css/app/root.tsx
@@ -0,0 +1,27 @@
+import { type LinksFunction } from '@remix-run/node'
+import { Links, LiveReload } from '@remix-run/react'
+import faviconAssetUrl from './assets/favicon.svg'
+import fontStylestylesheetUrl from './styles/font.css'
+import tailwindStylesheetUrl from './styles/tailwind.css'
+
+export const links: LinksFunction = () => {
+	return [
+		{ rel: 'icon', type: 'image/svg+xml', href: faviconAssetUrl },
+		{ rel: 'stylesheet', href: fontStylestylesheetUrl },
+		{ rel: 'stylesheet', href: tailwindStylesheetUrl },
+	]
+}
+
+export default function App() {
+	return (
+		<html lang="en">
+			<head>
+				<Links />
+			</head>
+			<body>
+				<p className="p-8 text-xl">Hello World</p>
+				<LiveReload />
+			</body>
+		</html>
+	)
+}
