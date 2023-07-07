28d66c02dede67dffe64592c0aa286b86ee7ce32
Kent C. Dodds <me+github@kentcdodds.com>
2023-07-07 10:12:04 -0600

    improve exercise 1

--- exercises/01.styling/03.problem.global-styles/app/root.tsx
+++ exercises/01.styling/03.problem.global-styles/app/root.tsx
@@ -2,9 +2,13 @@ import { KCDShopIFrameSync } from '@kentcdodds/workshop-app/iframe-sync'
 import { type LinksFunction } from '@remix-run/node'
 import { Links, LiveReload, Scripts } from '@remix-run/react'
 import faviconAssetUrl from './assets/favicon.svg'
+// 🐨 get the fontStylestylesheetUrl from the ./styles/font.css file
 
 export const links: LinksFunction = () => {
-	return [{ rel: 'icon', type: 'image/svg+xml', href: faviconAssetUrl }]
+	return [
+		{ rel: 'icon', type: 'image/svg+xml', href: faviconAssetUrl },
+		// 🐨 add a link for the fonts file here
+	]
 }
 
 export default function App() {
