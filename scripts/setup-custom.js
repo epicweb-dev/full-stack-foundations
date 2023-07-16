2bfebf1a141d4daed7f2bd3d60ba7d81aae82144
Kent C. Dodds <me+github@kentcdodds.com>
2023-07-15 22:36:07 -0600

    skip playwright and playground setup

--- scripts/setup-custom.js
+++ scripts/setup-custom.js
@@ -19,36 +19,40 @@ const uniqueApps = allApps.filter(
 )
 const problemApps = allApps.filter(isProblemApp)
 
-console.log(
-	'🎭 installing playwright for testing... This may require sudo (or admin) privileges and may ask for your password.',
-)
-try {
-	await $({
-		all: true,
-	})`npx playwright install chromium --with-deps`
-	console.log('✅ playwright installed')
-} catch (playwrightErrorResult) {
-	console.log(playwrightErrorResult.all)
-	throw new Error('❌  playwright install failed')
+if (!process.env.SKIP_PLAYWRIGHT) {
+	console.log(
+		'🎭 installing playwright for testing... This may require sudo (or admin) privileges and may ask for your password.',
+	)
+	try {
+		await $({
+			all: true,
+		})`npx playwright install chromium --with-deps`
+		console.log('✅ playwright installed')
+	} catch (playwrightErrorResult) {
+		console.log(playwrightErrorResult.all)
+		throw new Error('❌  playwright install failed')
+	}
 }
 
-const firstProblemApp = problemApps[0]
-if (firstProblemApp) {
-	console.log('🛝  setting up the first problem app...')
-	const playgroundPath = path.join(process.cwd(), 'playground')
-	if (await fsExtra.exists(playgroundPath)) {
-		console.log('🗑  deleting existing playground app')
-		await fsExtra.remove(playgroundPath)
+if (!process.env.SKIP_PLAYGROUND) {
+	const firstProblemApp = problemApps[0]
+	if (firstProblemApp) {
+		console.log('🛝  setting up the first problem app...')
+		const playgroundPath = path.join(process.cwd(), 'playground')
+		if (await fsExtra.exists(playgroundPath)) {
+			console.log('🗑  deleting existing playground app')
+			await fsExtra.remove(playgroundPath)
+		}
+		await setPlayground(firstProblemApp.fullPath).then(
+			() => {
+				console.log('✅ first problem app set up')
+			},
+			error => {
+				console.error(error)
+				throw new Error('❌  first problem app setup failed')
+			},
+		)
 	}
-	await setPlayground(firstProblemApp.fullPath).then(
-		() => {
-			console.log('✅ first problem app set up')
-		},
-		error => {
-			console.error(error)
-			throw new Error('❌  first problem app setup failed')
-		},
-	)
 }
 
 getWatcher().close()
