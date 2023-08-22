ad7524959200d32d2974372b826a0190e34361cd
Kent C. Dodds <me+github@kentcdodds.com>
2023-08-22 09:10:05 -0600

    improve instructions

--- exercises/02.routing/02.problem.links/app/routes/users+/kody_+/notes.tsx
+++ exercises/02.routing/02.problem.links/app/routes/users+/kody_+/notes.tsx
@@ -5,7 +5,7 @@ export default function NotesRoute() {
 		<div className="flex h-full justify-between pb-12 border-8 border-blue-500">
 			<h1 className="text-h1">Notes</h1>
 			{/* 🐨 add two links here.
-				One to go back to the parent path
+				One to go back to the parent *path* (💰 not parent "route" that's why you need to use the relative="path" prop)
 				and the other to go to the some-note-id route
 			*/}
 			{/* 💰 feel free to restructure things however you like to make them look nice */}
