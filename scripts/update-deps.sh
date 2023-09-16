b863df18cbe20b96c85af3c970384c28495aa6bc
Kent C. Dodds <me+github@kentcdodds.com>
2023-09-16 10:25:08 -0600

    remove exercise node modules too

--- scripts/update-deps.sh
+++ scripts/update-deps.sh
@@ -1,5 +1,5 @@
 npx npm-check-updates --dep prod,dev --upgrade --workspaces --root
-rm -rf node_modules package-lock.json
+rm -rf node_modules package-lock.json ./exercises/**/node_modules
 npm install
 node setup.js
 npm run typecheck
