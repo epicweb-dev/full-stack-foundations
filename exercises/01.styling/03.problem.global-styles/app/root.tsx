31caf29b2ae3c470fbc2668a63b8bf4c783afd1e
vondia <pimvdbosch@gmail.com>
2023-10-24 13:26:10 +0200

    exercise 3 done

--- exercises/01.styling/03.problem.global-styles/app/root.tsx
+++ exercises/01.styling/03.problem.global-styles/app/root.tsx
@@ -2,12 +2,12 @@ import { type LinksFunction } from '@remix-run/node'
 import { Links, LiveReload, Scripts } from '@remix-run/react'
 import faviconAssetUrl from './assets/favicon.svg'
 import { KCDShop } from './kcdshop.tsx'
-// 🐨 get the fontStylesheetUrl from the ./styles/font.css file
+import fontStylesheetUrl from './styles/font.css'
 
 export const links: LinksFunction = () => {
 	return [
 		{ rel: 'icon', type: 'image/svg+xml', href: faviconAssetUrl },
-		// 🐨 add a link for the fonts file here
+		{ rel: 'stylesheet', href: fontStylesheetUrl },
 	]
 }
 
