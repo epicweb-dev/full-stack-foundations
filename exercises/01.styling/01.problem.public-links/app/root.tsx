86aaf91ffb1f3fc3d105a9a8e0de8b5077b2feef
vondia <pimvdbosch@gmail.com>
2023-10-24 13:10:07 +0200

    exercise 1 done

--- exercises/01.styling/01.problem.public-links/app/root.tsx
+++ exercises/01.styling/01.problem.public-links/app/root.tsx
@@ -1,16 +1,17 @@
-import { LiveReload, Scripts } from '@remix-run/react'
+import { type LinksFunction } from '@remix-run/node'
+import { Links, LiveReload, Scripts } from '@remix-run/react'
 import { KCDShop } from './kcdshop.tsx'
 
-// 🐨 export a links function here that adds the favicon
-// 💰 It should have the following properties:
-// - rel: 'icon'
-// - type: 'image/svg+xml'
-// - href: '/favicon.svg'
+export const links: LinksFunction = () => {
+	return [{ rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }]
+}
 
 export default function App() {
 	return (
 		<html lang="en">
-			<head>{/* 🐨 Put Remix's <Links /> in here */}</head>
+			<head>
+				<Links />
+			</head>
 			<body>
 				<p>Hello World</p>
 				<Scripts />
