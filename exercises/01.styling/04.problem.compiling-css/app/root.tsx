284fef3a25b3e8ab45168ca444a4c4a8424594b6
vondia <pimvdbosch@gmail.com>
2023-10-24 13:38:53 +0200

    excercise 4 done

--- exercises/01.styling/04.problem.compiling-css/app/root.tsx
+++ exercises/01.styling/04.problem.compiling-css/app/root.tsx
@@ -3,13 +3,13 @@ import { Links, LiveReload, Scripts } from '@remix-run/react'
 import faviconAssetUrl from './assets/favicon.svg'
 import { KCDShop } from './kcdshop.tsx'
 import fontStylesheetUrl from './styles/font.css'
-// 🐨 Import the tailwind stylesheet here
+import tailwindStylesheetUrl from './styles/tailwind.css'
 
 export const links: LinksFunction = () => {
 	return [
 		{ rel: 'icon', type: 'image/svg+xml', href: faviconAssetUrl },
 		{ rel: 'stylesheet', href: fontStylesheetUrl },
-		// 🐨 Add the tailwind stylesheet to the links array here
+		{ rel: 'stylesheet', href: tailwindStylesheetUrl },
 	]
 }
 
@@ -20,11 +20,7 @@ export default function App() {
 				<Links />
 			</head>
 			<body>
-				{/*
-					🐨 add a className with tailwind classes to this p tag to check that
-					tailwind is processing correctly. Try `p-8 text-xl` for example.
-				*/}
-				<p>Hello World</p>
+				<p className="p-8 text-xl">Hello World</p>
 				<Scripts />
 				<KCDShop />
 				<LiveReload />
