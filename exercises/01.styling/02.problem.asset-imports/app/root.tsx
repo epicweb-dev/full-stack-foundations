59e73f6d3b1328776cde24510f7c72aed8822caa
vondia <pimvdbosch@gmail.com>
2023-10-24 13:19:07 +0200

    exercise 2 done

--- exercises/01.styling/02.problem.asset-imports/app/root.tsx
+++ exercises/01.styling/02.problem.asset-imports/app/root.tsx
@@ -1,11 +1,11 @@
 import { type LinksFunction } from '@remix-run/node'
 import { Links, LiveReload, Scripts } from '@remix-run/react'
-// 🐨 import the svg favicon here (use a default import)
+import faviconAssetUrl from './assets/favicon.svg'
+
 import { KCDShop } from './kcdshop.tsx'
 
 export const links: LinksFunction = () => {
-	// 🐨 swap the hard-coded href here with the default import of the favicon
-	return [{ rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }]
+	return [{ rel: 'icon', type: 'image/svg+xml', href: faviconAssetUrl }]
 }
 
 export default function App() {
