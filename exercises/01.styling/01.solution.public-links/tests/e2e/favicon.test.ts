52122e482d1f38d66ce11e8e7012fbe2ef60f343
kivvvvv <sirapop.iam.0131@gmail.com>
2024-05-11 10:11:37 +0700

    use truthy assertion againsts undefined and null value

--- exercises/01.styling/01.solution.public-links/tests/e2e/favicon.test.ts
+++ exercises/01.styling/01.solution.public-links/tests/e2e/favicon.test.ts
@@ -3,6 +3,6 @@ import { test, expect } from '@playwright/test'
 test('checks favicon', async ({ page }) => {
 	await page.goto('/')
 	const favicon = await page.$('link[rel="icon"]')
-	const href = await favicon?.getAttribute('href') ?? null
-	expect(href, `🚨 The favicon link href cannot be found`).not.toBeNull()
+	const href = await favicon?.getAttribute('href')
+	expect(href, `🚨 The favicon link href cannot be found`).toBeTruthy()
 })
